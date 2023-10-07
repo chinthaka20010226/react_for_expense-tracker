8f68bfd04a8f794d3daff8258961ae5bf58211bb
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-10-07 09:04:56 +0530

    adding hooks

--- src/components/AddNewTransactionList.js
+++ src/components/AddNewTransactionList.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddNewTransactionList = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
   return (
-    <div class="container">
+    <div className="container">
         <h3>Add new transaction</h3>
         <form id="form">
-            <div class="form-control">
-                <label for="text">Text</label>
-                <input type="text" id="text" placeholder='Enter text...'></input>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" id="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'></input>
             </div>
-            <div class="form-control">
-                <label for="amount">Amount <br />
+            <div className="form-control">
+                <label htmlFor="amount">Amount <br />
                 (ingative - expense, positive - income)</label>
-                <input type="number" id="amount" placeholder='Enter amount...'></input>
+                <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Enter amount...'></input>
             </div>
-            <button class="btn">Add transaction</button>
+            <button className="btn">Add transaction</button>
         </form>
     </div>
   )
